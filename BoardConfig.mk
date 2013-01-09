a060496be533c11a928b697570372111fc6f87c7
ThaiScopad <root@ubuntu.(none)>
2013-01-09 03:52:59 -0800

    Add TARGET_ARCH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH := arm
 ARCH_ARM_HAVE_VFP := true
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_ARCH_VARIANT_FPU := neon
